abbc0786e2dfb838af46dc99fc071d48e01b1d6c
Angela <kafuigalley7@gmail.com>
2024-01-10 06:30:51 +0000

    Complete implementation of app components:  With this commit, I finished implementing the main components for the blog app. The Home component and other relevant UI elements are now in place, providing the necessary structure and content for the initial version of the application.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hi there! Welcome to my blog, Dev notes!</h1>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Ad adipisicing aliquip consectetur veniam non deserunt nisi id ad reprehenderit consectetur laboris. Nulla nostrud deserunt irure eiusmod tempor. Veniam sunt ad nostrud nisi minim Lorem ut Lorem nisi non commodo occaecat. Dolor do esse aliquip sint amet cillum do Lorem laborum in velit ea occaecat. Non exercitation aliqua ex commodo elit Lorem et non esse pariatur tempor excepteur cillum labore. Ullamco consectetur quis velit dolor reprehenderit labore ex tempor consectetur aliqua.</p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>In officia elit Lorem proident tempor commodo mollit laboris ipsum tempor elit duis cillum. Ullamco aliqua aliquip reprehenderit minim est dolor duis elit laborum cupidatat quis tempor ut. Adipisicing aliquip labore labore aliqua sint amet aliqua sunt cillum laborum velit.</p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Enim ex laborum excepteur fugiat mollit minim minim ad amet et ea. Fugiat minim irure laboris labore irure enim duis ut magna velit id. Sint ut ea cillum ex incididunt nisi consequat deserunt Lorem Lorem ullamco. Aliqua officia ipsum reprehenderit Lorem irure cillum fugiat adipisicing ex. Ea aute voluptate irure commodo laborum.</p>
+        </div>
+    )
+}
+
+export default Home
