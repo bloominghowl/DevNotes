701b9ddb5b21de832912c8673d8b579789674b4f
Angela <kafuigalley7@gmail.com>
2024-01-10 13:03:42 +0000

    Create 'article-component.js': In this commit, I added a new component, 'article-component.js', to handle the display of individual articles using URL parameters with React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -18,11 +18,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About/>} />
           <Route path="/articles-list" element={<ArticlesList />} />
-          <Route path="/article" element={<Article />} />
+          <Route path="/article/:name" element={<Article />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
