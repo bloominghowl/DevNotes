5617ffb51ce50e1b3de5bf563e6c94d180b0ee03
Angela <kafuigalley7@gmail.com>
2024-01-10 15:28:17 +0000

    Update Navbar color and create Articles component: Changed the color of the Navbar for improved visibility. Created the 'Articles.js' component to provide readers with links to other articles after finishing the current one.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,16 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className='border-b-4 border-green-100 text-center fixed top-0 bg-green-500 font-bold w-full text-lg text-white'>
-            <ul className='py-4'>
+        <nav className='border-b-4 border-green-100 text-center fixed top-0 bg-indigo-700 font-bold w-full text-lg text-white'>
+            <ul className='py-4 '>
                 <li className='inline-block'>
-                    <Link to="/" className='pl-6 pr-8'>Home</Link>
+                    <Link to="/" className='pl-6 pr-8 inline-block no-underline hover:text-indigo-200 font-medium text-lg py-2 px-4 lg:-ml-2'>
+                        Home
+                    </Link>
                 </li>
                 <li className='inline-block'>
-                    <Link to="/about" className='pl-6 pr-8'>About</Link>
+                    <Link to="/articles-list" className='pl-6 pr-8 pl-6 pr-8 inline-block no-underline hover:text-indigo-200 font-medium text-lg py-2 px-4 lg:-ml-2'>
+                        Articles
+                    </Link>
                 </li>
                 <li className='inline-block'>
-                    <Link to="/articles-list" className='pl-6 pr-8'>Articles</Link>
+                    <Link to="/about" className='pl-6 pr-8 pl-6 pr-8 inline-block no-underline hover:text-indigo-200 font-medium text-lg py-2 px-4 lg:-ml-2'>
+                        About
+                    </Link>
                 </li>
             </ul>
         </nav>
